61427950955b979489f8d58210f7452d36898d07
Joerg322 <root@assetmanager.bsi.corp>
2025-07-19 12:37:49 +0000

    install continued

--- install-MatchingAgent.bash
+++ install-MatchingAgent.bash
@@ -11,19 +11,11 @@
 
 echo "Installation MatchingAgent starting..."
 
-git config user.email "root@assetmanager.bsi.corp"
-
 apt install -y postgresql
 systemctl start postgresql
 systemctl enable postgresql
 
 sudo -i -u postgres psql -f /home/Matching-Agent/initdb.sql
-
-# apt-get -y install postgresql postgresql-contrib
-# cp /home/Matching-Agent/pg_hba.conf /etc/postgresql/16/main/
-# service postgresql start
-# sudo -i -u postgres createuser -d -e -s -P analyst
-# sudo -i -u postgres createdb -e cachedb
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
